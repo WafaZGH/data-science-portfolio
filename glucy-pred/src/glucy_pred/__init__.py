1dacfb689c21fe4a1941f2a5755515eea49dd4cd
WafaZGH <w.zeghouane@gmail.com>
2025-09-15 11:48:02 +0200

    Create __init__.py

---
+++ glucy-pred/src/glucy_pred/__init__.py
@@ -0,0 +1,4 @@
+from .weight_estimation import enrichir_avec_poids
+from .gi_loader import load_gi_table, get_gi_by_name
+from .nutrition_loader import load_ciqual, get_row_by_name
+from .nutrition import filter_food_rows
